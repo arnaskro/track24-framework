35ecd13da93c7d3d551689956c0977418975922d
Arnas Kromelis <arnas.kromelis@gmail.com>
2018-07-03 13:18:09 +0400

    removed lazy loading css

--- src/index.js
+++ src/index.js
@@ -2,17 +2,6 @@ import './index.css'
 import Button from './components/button/';
 import Input from './components/form/Input/';
 import CardBasic from './components/cardBasic';
-import { loadCSS } from 'fg-loadcss';
-
-// if in production, lazy load css
-if (process.env.NODE_ENV === 'production') {
-  if (!window.loadCSS) {
-    window.loadCSS = loadCSS;
-  }
-  
-  // Lazy load CSS
-  loadCSS(process.env.REACT_APP_CDN + process.env.REACT_APP_REMOTE_FOLDER + '/main.css');
-}
 
 export {
   Button,
