2ea736c122345f539f8cdeba6d268c3075542a81
yasmine <yasmine.fadel@track24.com>
2018-07-04 13:54:09 +0400

    exporting components oops

--- src/index.js
+++ src/index.js
@@ -1,16 +1,31 @@
 import './index.css'
 import Button from './components/button/';
 import Input from './components/form/Input/';
+import Toast from './components/toast/';
 import CardBasic from './components/cardBasic';
+import CardInspection from './components/cardInspection';
+import CardPoll from './components/cardPoll';
+import CardTravelWarning from './components/cardTravelWarning';
+import CardLocation from './components/cardLocation';
 
 export {
   Button,
   Input,
-  CardBasic
+  CardBasic,
+  CardInspection,
+  CardPoll,
+  CardTravelWarning,
+  CardLocation,
+  Toast
 };
 
 export default {
   Button,
   Input,
-  CardBasic
+  CardBasic,
+  CardInspection,
+  CardPoll,
+  CardTravelWarning,
+  CardLocation,
+  Toast
 };
